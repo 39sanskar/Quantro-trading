88d1a578fc38a0b1e710e7b6bea224b058369ce9
39sanskar <mishrasanskar496@gmail.com>
2026-05-10 19:39:08 +0530

    config-files

---
+++ quantro/src/main/java/com/sanskar/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.sanskar.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "5d2d6f8d5a8f6d5a8f6d5a8f6d5a8f6d5a8f6d5a8f6d5a8f6d5a8f6d5a8f6d5a"; // // 64 hex chars = 256 bits
+    public static final String JWT_HEADER = "Authorization";
+}
+
+
